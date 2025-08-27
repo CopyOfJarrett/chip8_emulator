d956685041aa86fc3f38c1958adf7b4705dc6969
CopyOfJarrett <131999553+CopyOfJarrett@users.noreply.github.com>
2025-08-27 06:11:26 +0000

    .

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub use rand::rng;
 
 pub mod modules;
 pub use modules::*;
+
+
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -4,3 +4,9 @@ pub use memory::*;
 
 pub mod cpu;
 pub use cpu::*;
+
+pub mod keyboard;
+pub use keyboard::*;
+
+pub mod display;
+pub use display::*;
